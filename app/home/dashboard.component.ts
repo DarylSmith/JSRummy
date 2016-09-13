34f18102db0f0e3da1363be0d9c8cf7919c988e0
Daryl Smith <darylsmith@lifespeak.com>
2016-09-12 22:08:04 -0400

    added styles and buttons to main page

--- app/home/dashboard.component.ts
+++ app/home/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component, HostBinding } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 
 @Component({
-    templateUrl: 'app/home/dashboard.component.html'
+    templateUrl: 'app/home/dashboard.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
 export class DashboardComponent {
     public pageTitle: string = 'Dashboard';
