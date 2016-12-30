31c7d56819a6f9b699c6cb1bc56dd6c21404d35f
Daryl Smith <darylsmith@lifespeak.com>
2016-12-30 12:31:20 -0500

    fixes for pipe sorting

--- src/pipes/SortHandPipe.ts
+++ src/pipes/SortHandPipe.ts
@@ -5,6 +5,7 @@ import {Card, Hand, Deck} from '../providers/jrummy/jrummy'
 @Pipe({ name: 'SortHand' })
 export class SortHandPipe implements PipeTransform {
   transform(hand: Hand) {
+    console.log(hand);
     hand.sortByValue();
     return hand.Cards;
   }
